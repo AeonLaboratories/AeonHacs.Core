116959fb25a489e77d6bd1fbb26b5755953ac910
Tyler Wilson <tjwilson.91@gmail.com>
2025-03-06 12:24:13 -0700

    Timeout is now correctly calculated while Stopping.

--- Components/StateManager.cs
+++ Components/StateManager.cs
@@ -119,13 +119,12 @@ namespace AeonHacs.Components
                     // vary over multiple calls when it depends on changing
                     // conditions.
                     var timeout = StateLoopTimeout;
-                    if (timeout < 0) timeout = Timeout.Infinite;
+                    if (timeout < 0) timeout = Stopping ? 0 : Timeout.Infinite;
 
                     if (LogEverything)
                         Log?.Record($"StateManager {Name}: StateLoop is waiting for StateSignal" +
                             (timeout == Timeout.Infinite ? "..." : $" or {timeout} ms..."));
 
-                    if (Stopping) timeout = Math.Min(timeout, 30);
                     StateSignalReceived = StateSignal.WaitOne(timeout);
 
                     if (LogEverything)
